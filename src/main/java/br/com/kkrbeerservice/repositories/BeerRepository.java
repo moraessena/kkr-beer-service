f3b2f400294890d21e16ced5676d62964de64749
daniel sena <danielsena04@gmail.com>
2021-08-09 20:56:11 -0300

    feat: add beer repository. closes #5

---
+++ src/main/java/br/com/kkrbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package br.com.kkrbeerservice.repositories;
+
+import br.com.kkrbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
