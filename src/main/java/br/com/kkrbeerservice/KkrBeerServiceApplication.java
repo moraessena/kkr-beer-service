a80f926261894fa783ec95f1d88fae9423658769
daniel sena <danielsena04@gmail.com>
2021-08-08 22:22:17 -0300

    first commit

---
+++ src/main/java/br/com/kkrbeerservice/KkrBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.kkrbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkrBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkrBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kkrbeerservice/KkrBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kkrbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkrBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
