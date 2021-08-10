f29c324cc2162f4660aad606e601e1762b19d48e
daniel sena <danielsena04@gmail.com>
2021-08-09 23:29:18 -0300

    feat: add mapstruct. closes #9

---
+++ src/main/java/br/com/kkrbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,18 @@
+package br.com.kkrbeerservice.web.mappers;
+
+import br.com.kkrbeerservice.domain.Beer;
+import br.com.kkrbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public interface BeerMapper {
+
+    @Mapping(source = "name", target = "beerName")
+    @Mapping(source = "style", target = "beerStyle")
+    BeerDto beertoBeerDto(Beer beer);
+
+    @Mapping(source = "beerName", target = "name")
+    @Mapping(source = "beerStyle", target = "style")
+    Beer beerDtoToBeer(BeerDto beeroDto);
+}
