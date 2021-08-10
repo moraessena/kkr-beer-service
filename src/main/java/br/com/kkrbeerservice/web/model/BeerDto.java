c215c7312f261d6a8b689a899f6774e5f0cb227c
daniel sena <danielsena04@gmail.com>
2021-08-09 22:04:22 -0300

    feat: add bean validation to beerdto. closes #7

--- src/main/java/br/com/kkrbeerservice/web/model/BeerDto.java
+++ src/main/java/br/com/kkrbeerservice/web/model/BeerDto.java
@@ -2,6 +2,10 @@ package br.com.kkrbeerservice.web.model;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -13,15 +17,25 @@ import java.util.UUID;
 @EqualsAndHashCode(of = {"id"})
 @ToString(of = {"id", "beerName"})
 public class BeerDto {
+    @Null
     private UUID id;
+    @NotBlank
     private String beerName;
+    @NotNull
     private Style beerStyle;
+    @NotNull
+    @Positive
     private Long upc;
+    @NotNull
+    @Positive
     private BigDecimal price;
     private Integer stockQuantity;
 
+    @Null
     private Integer version;
+    @Null
     private LocalDateTime createdAt;
+    @Null
     private LocalDateTime updatedAt;
 
     public enum Style {
