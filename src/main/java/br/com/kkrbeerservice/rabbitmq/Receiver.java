b724c4f234be84e9e02d251c5dc609610434aebb
daniel sena <danielsena04@gmail.com>
2021-08-28 15:53:19 -0300

    config: add rabbitmq configuration

---
+++ src/main/java/br/com/kkrbeerservice/rabbitmq/Receiver.java
@@ -0,0 +1,10 @@
+package br.com.kkrbeerservice.rabbitmq;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+    public void receiveMessage(String message) {
+        System.out.println(message);
+    }
+}
